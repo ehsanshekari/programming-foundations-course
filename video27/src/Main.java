fd0ace2a732bb5a51ade9b666a5ed94b8e0beee5
unknown <e.shekari@gmail.com>
2019-08-09 18:36:22 +0430

    video27

---
+++ video27/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int remainder = a % 2;
+
+        if( remainder == 0){
+            System.out.println("Number is even.");
+        }
+
+        if (remainder != 0){
+            System.out.println("Number is odd.");
+        }
+
+    }
+}
