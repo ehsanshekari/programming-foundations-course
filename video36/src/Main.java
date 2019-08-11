5fac6012c868771ff23f2cd9df2f651c0468e452
unknown <e.shekari@gmail.com>
2019-08-11 21:48:30 +0430

    video36

---
+++ video36/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // double[] grades = {12, 17, 15.5, 18.25};
+
+        double[] grades = new double[4];
+        grades[0] = 12;
+        grades[1] = 17;
+        grades[2] = 15.5;
+        grades[3] = 18.25;
+
+        System.out.println("Array length is:" + grades.length);
+
+    }
+}
